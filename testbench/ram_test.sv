6576b14ed3eca01ccb194763da0276647cf76b96
Neha-Sahana <126313169+Neha-Sahana@users.noreply.github.com>
2026-07-07 09:25:49 +0530

    ram test code
    Refactored ram_scoreboard class into ram_test class and added multiple test classes for different transactions.

--- testbench/ram_test.sv
+++ testbench/ram_test.sv
@@ -1,49 +1,144 @@
-`include "defines.sv"
-class ram_scoreboard;
-   ram_transaction ref2sb_trans,mon2sb_trans;
-   mailbox #(ram_transaction) mbx_rs;
-   mailbox #(ram_transaction) mbx_ms;
-   bit [7:0] ref_mem [bit [31:0]]; 
-   bit [7:0] mon_mem [bit [31:0]]; 
-   int MATCH,MISMATCH;
+class ram_test;
+  virtual ram_if drv_vif;
+  virtual ram_if mon_vif;
+  virtual ram_if ref_vif;
+  ram_environment env;
 
-  function new(mailbox #(ram_transaction) mbx_rs,
-               mailbox #(ram_transaction) mbx_ms);
-    this.mbx_rs=mbx_rs;
-    this.mbx_ms=mbx_ms;
+  function new(virtual ram_if drv_vif,
+               virtual ram_if mon_vif,
+               virtual ram_if ref_vif);
+    this.drv_vif=drv_vif;
+    this.mon_vif=mon_vif;
+    this.ref_vif=ref_vif;
   endfunction
 
-  task start();
-    for(int i=0;i<`no_of_trans;i++)
-      begin
-        ref2sb_trans=new();
-        mon2sb_trans=new();
-          begin
-           mbx_rs.get(ref2sb_trans);
-           ref_mem[ref2sb_trans.address]=ref2sb_trans.data_out;
-           $display("****SCOREBOARD REFdata_out=%0d, ADDRESS=%d*****",ref_mem[ref2sb_trans.address],ref2sb_trans.address,$time);
-          end
-          begin
-           mbx_ms.get(mon2sb_trans);
-           mon_mem[mon2sb_trans.address]=mon2sb_trans.data_out;
-           $display("***SCOREBOARD MONdata_out=%0d,  ADDRESS=%d***",mon_mem[mon2sb_trans.address],mon2sb_trans.address,$time);
-          end
-          compare_report();
-      end
+  task run();
+    env=new(drv_vif,mon_vif,ref_vif);
+    env.build;
+    env.start;
   endtask
-
-task compare_report();
-     if(ref_mem[ref2sb_trans.address] == mon_mem[mon2sb_trans.address])
-          begin
-            $display("SCOREBOARD REFdata_out=%0d, MONdata_out=%0d",ref_mem[ref2sb_trans.address],mon_mem[mon2sb_trans.address],$time);
-            MATCH++;
-            $display("DATA MATCH SUCCESSFUL MATCH=%d",MATCH);
-          end
-        else
-          begin
-            $display("SCOREBOARD REFdata_out=%0d, MONdata_out=%0d",ref_mem[ref2sb_trans.address],mon_mem[mon2sb_trans.address],$time);
-            MISMATCH++;
-            $display("DATA MATCH FAILED MISMATCH=%d",MISMATCH);
-          end
-endtask
 endclass
+
+class test1 extends ram_test;
+ ram_transaction1 trans;
+  function new(virtual ram_if drv_vif,virtual ram_if mon_vif, virtual ram_if ref_vif);
+    super.new(drv_vif,mon_vif,ref_vif);
+  endfunction
+
+  task run();
+    env=new(drv_vif,mon_vif,ref_vif);
+    env.build;
+    begin 
+    trans = new();
+    env.gen.blueprint= trans;
+    end
+    env.start;
+  endtask
+endclass
+
+class test2 extends ram_test;
+ ram_transaction2 trans;
+  function new(virtual ram_if drv_vif,virtual ram_if mon_vif,virtual ram_if ref_vif);
+    super.new(drv_vif,mon_vif,ref_vif);
+  endfunction
+
+  task run();
+    $display("child test");
+    env=new(drv_vif,mon_vif,ref_vif);
+    env.build;
+    begin 
+    trans = new();
+    env.gen.blueprint= trans;
+    end
+    env.start;
+  endtask
+endclass
+
+class test3 extends ram_test;
+ ram_transaction3 trans;
+  function new(virtual ram_if drv_vif,virtual ram_if mon_vif,virtual ram_if ref_vif);
+    super.new(drv_vif,mon_vif,ref_vif);
+  endfunction
+
+  task run();
+    $display("child test");
+    env=new(drv_vif,mon_vif,ref_vif);
+    env.build;
+    begin 
+    trans = new();
+    env.gen.blueprint= trans;
+    end
+    env.start;
+  endtask
+endclass
+
+class test4 extends ram_test;
+ ram_transaction4 trans;
+  function new(virtual ram_if drv_vif,virtual ram_if mon_vif,virtual ram_if ref_vif);
+    super.new(drv_vif,mon_vif,ref_vif);
+  endfunction
+
+  task run();
+    $display("child test");
+    env=new(drv_vif,mon_vif,ref_vif);
+    env.build;
+    begin 
+    trans = new();
+    env.gen.blueprint= trans;
+    end
+    env.start;
+  endtask
+endclass
+
+
+class test_regression extends ram_test;
+ ram_transaction  trans0;
+ ram_transaction1 trans1;
+ ram_transaction2 trans2;
+ ram_transaction3 trans3;
+ ram_transaction4 trans4;
+  function new(virtual ram_if drv_vif,
+               virtual ram_if mon_vif,
+               virtual ram_if ref_vif);
+    super.new(drv_vif,mon_vif,ref_vif);
+  endfunction
+
+  task run();
+    $display("child test");
+    env=new(drv_vif,mon_vif,ref_vif);
+    env.build;
+
+    begin 
+    trans0 = new();
+    env.gen.blueprint= trans0;
+    end
+    env.start;
+
+    begin 
+    trans1 = new();
+    env.gen.blueprint= trans1;
+    end
+    env.start;
+
+    begin 
+    trans2 = new();
+    env.gen.blueprint= trans2;
+    end
+    env.start;
+
+    begin 
+    trans3 = new();
+    env.gen.blueprint= trans3;
+    end
+    env.start;
+
+    begin 
+    trans4 = new();
+    env.gen.blueprint= trans4;
+    end
+    env.start;
+
+  endtask
+endclass
+
+
