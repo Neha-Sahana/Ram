f2ae45a7bc94ebe51c154c05574ceb4a285cf371
Neha-Sahana <126313169+Neha-Sahana@users.noreply.github.com>
2026-07-07 09:29:29 +0530

    top module

---
+++ testbench/top.sv
@@ -0,0 +1,54 @@
+`include "ram.sv"
+`include "ram_if.sv"
+`include "ram_pkg.sv"
+module top( );
+    import ram_pkg ::*; 
+    bit clk;
+    bit reset;
+
+  initial
+    begin
+     forever #10 clk=~clk;
+    end
+  initial
+    begin
+      @(posedge clk);
+      reset=0;
+      repeat(1)@(posedge clk);
+      reset=1;
+      @(posedge clk);
+      reset=0;
+      repeat(2)@(posedge clk);
+      reset=1;
+    end
+ 
+
+    ram_if intrf(clk,reset);
+    RAM DUV(.data_in(intrf.data_in),
+            .write_enb(intrf.write_enb),
+            .read_enb(intrf.read_enb),
+            .data_out(intrf.data_out),
+            .address(intrf.address),
+            .clk(clk),
+            .reset(reset)
+           );
+    ram_test tb= new(intrf.DRV,intrf.MON,intrf.REF_SB);
+    test1 tb1= new(intrf.DRV,intrf.MON,intrf.REF_SB);
+    test2 tb2= new(intrf.DRV,intrf.MON,intrf.REF_SB);
+    test3 tb3= new(intrf.DRV,intrf.MON,intrf.REF_SB);
+    test4 tb4= new(intrf.DRV,intrf.MON,intrf.REF_SB);
+    test_regression tb_regression= new(intrf.DRV,intrf.MON,intrf.REF_SB);
+
+  initial
+   begin
+    tb1.run();
+    tb2.run();
+    tb3.run();
+    tb4.run();
+    tb_regression.run();
+    tb.run();
+    $finish();
+   end
+    
+endmodule
+
